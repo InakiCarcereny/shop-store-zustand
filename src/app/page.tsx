d850c41f6891ad61faf496d3bfeda01eb5a334eb
inakicarcereny <inakicarce@hotmail.com.ar>
2024-11-16 23:31:38 -0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import { Counter } from "./components";
+import { Header, MainContent, MovementPhrase } from "./components";
 
 export default function Home() {
   return (
-    <>
-      <Counter />
-    </>
+    <div className="w-screen h-screen flex flex-col gap-14 py-6 px-14">
+      <Header />
+
+      <MovementPhrase />
+
+      <MainContent />
+    </div>
   );
 }
