6092b72675d826e54c0051eebb070c8785a415ca
inakicarcereny <inakicarce@hotmail.com.ar>
2024-11-17 22:01:46 -0300

    Add getProduct function to fetch products by title from the API for enhanced product retrieval functionality

--- src/app/services/products.ts
+++ src/app/services/products.ts
@@ -1,4 +1,7 @@
 import axios from "axios";
 
+export const getProduct = async (title: string) =>
+  axios.get(`https://api.escuelajs.co/api/v1/products/?title=${title}`);
+
 export const getProducts = async () =>
   axios.get("https://api.escuelajs.co/api/v1/products");
