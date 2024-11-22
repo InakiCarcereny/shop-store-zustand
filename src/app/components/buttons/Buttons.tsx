3c9a92fb0d661db9673bef36642b7b605b786d4a
inakicarcereny <inakicarce@hotmail.com.ar>
2024-11-22 12:32:20 -0300

    Refactor Buttons component to use Next.js hooks for search parameter handling and update search input logic accordingly

--- src/app/components/buttons/Buttons.tsx
+++ src/app/components/buttons/Buttons.tsx
@@ -1,53 +1,37 @@
 "use client";
 
-import { Arrows, Search } from "@/app/icons";
-import { useForm } from "react-hook-form";
-import { getProduct } from "@/app/services";
-import { useState } from "react";
-import { Product } from "@/app/models";
-
-interface FormData {
-  search: string;
-}
+import { Search } from "@/app/icons";
+import { usePathname, useSearchParams, useRouter } from "next/navigation";
 
 export function Buttons() {
-  const [product, setProduct] = useState<Product | null>(null);
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormData>();
+  const handleSearch = (term: string) => {
+    const params = new URLSearchParams(searchParams);
 
-  const onSubmit = handleSubmit(async (data) => {
-    const res = await getProduct(data.search);
-    setProduct(res.data);
-  });
+    if (term) {
+      params.set("query", term);
+    } else {
+      params.delete("query");
+    }
 
-  console.log(product);
+    replace(`${pathname}?${params.toString()}`);
+  };
 
   return (
-    <div className="flex items-ceter justify-center flex-col sm:flex-row gap-6 w-full">
-      <form
-        onSubmit={onSubmit}
-        className="bg-zinc-100 rounded-[4px] px-2 py-2 flex items-center justify-between w-[250px]"
-      >
+    <div className="flex items-ceter justify-center sm:flex-row gap-6 w-full">
+      <div className="bg-zinc-100 rounded-[4px] px-2 py-2 flex items-center justify-between w-[450px]">
         <input
           type="search"
           className="bg-transparent w-full text-sm focus:outline-none"
           placeholder="Search..."
-          {...register("search")}
+          onChange={(e) => handleSearch(e.target.value)}
+          defaultValue={searchParams.get("query")?.toString()}
         />
         <Search />
-      </form>
-
-      <button
-        onClick={() => console.log("sort")}
-        className="flex items-center justify-center gap-2 bg-zinc-100 px-4 py-1 rounded-[4px] w-[100px]"
-      >
-        Sort
-        <Arrows />
-      </button>
+      </div>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
 import { Footer, Header, MainContent, MovementPhrase } from "./components";
 
-export default function Home() {
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  const query =
+    typeof searchParams.query === "string" ? searchParams.query : "";
+
   return (
     <div className="w-screen h-screen flex flex-col gap-14 py-6 px-14">
       <Header />
 
       <MovementPhrase />
 
-      <MainContent />
+      <MainContent query={query} />
 
       <Footer />
     </div>
