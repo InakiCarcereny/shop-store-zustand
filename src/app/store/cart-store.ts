3f6a64e7116046bd1d72fd1706b4d56c8e572e2a
inakicarcereny <inakicarce@hotmail.com.ar>
2024-11-22 12:32:13 -0300

    Add addQuantity and lessQuantity methods to CartStore for better quantity management of cart items

--- src/app/store/cart-store.ts
+++ src/app/store/cart-store.ts
@@ -5,6 +5,8 @@ interface CartStore {
   addItem: (item: Item) => void;
   removeItem: (item: Item) => void;
   clearCart: () => void;
+  addQuantity: (item: Item, quantity: number) => void;
+  lessQuantity: (item: Item) => void;
 }
 
 interface Item {
@@ -12,16 +14,36 @@ interface Item {
   title: string;
   price: number;
   description: string;
+  quantity: number;
   images: string[];
 }
 
 export const useCartStore = create<CartStore>((set) => ({
   cartItems: [],
   addItem: (item: Item) =>
-    set((state) => ({ cartItems: [...state.cartItems, item] })),
+    set((state) => ({
+      cartItems: [
+        ...state.cartItems,
+        { ...item, quantity: item.quantity || 1 },
+      ],
+    })),
   removeItem: (item: Item) =>
     set((state) => ({
       cartItems: state.cartItems.filter((cartItem) => cartItem.id !== item.id),
     })),
   clearCart: () => set({ cartItems: [] }),
+  addQuantity: (item: Item, quantity: number) =>
+    set((state) => ({
+      cartItems: state.cartItems.map((cartItem) =>
+        cartItem.id === item.id ? { ...cartItem, quantity } : cartItem
+      ),
+    })),
+  lessQuantity: (item: Item) =>
+    set((state) => ({
+      cartItems: state.cartItems.map((cartItem) =>
+        cartItem.id === item.id
+          ? { ...cartItem, quantity: cartItem.quantity - 1 }
+          : cartItem
+      ),
+    })),
 }));
